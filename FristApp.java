c93bc8fcdb50c7afe2ede2685d33f168e877d48a
sandipan08 <sandipan.sau08@gmail.com>
2019-10-17 19:25:00 +0530

    Sandipan Sau

--- FristApp.java
+++ FristApp.java
@@ -1,5 +1,18 @@
-class FristApp{
-    public static void main(String[] args) {
-        System.out.println("Helo world");
-    }
+package prtc;
+import java.util.Scanner;
+public class addno 
+	  {
+		   public static void main(String[] args) {
+		        Test ob=new Test();
+		        ob.add(10,20);
+	   }
+
+}
+
+class Test{
+	void add(int a, int b) {
+		int c=a+b;
+		System.out.println(c);
+	}
+
 }
